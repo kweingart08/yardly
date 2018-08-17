f238748b9e1a1ed59f82a666dec96bb4c03af388
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-08-17 12:46:55 -0400

    users index route added

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User
+
+  # ==================================================
+  #                      SET UP
+  # ==================================================
+  # connect to postgres
+  if(ENV['DATABASE_URL'])
+     uri = URI.parse(ENV['DATABASE_URL'])
+     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+     DB = PG.connect(host: "localhost", port: 5432, dbname: 'yardly_development')
+  end
+
+  # ==================================================
+  #                      ROUTES
+  # ==================================================
+
+  # get all
+  def self.all
+    results = DB.exec("SELECT * FROM users;")
+    return results.map do |result|
+      {
+        "id" => result["id"].to_i,
+        "username" => result["username"],
+        "password" => result["password"],
+        "address" => result["address"]
+      }
+    end
+  end
+
+
+end
