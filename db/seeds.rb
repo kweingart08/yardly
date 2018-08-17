4aaba25a62cc2cac647cbc160e6e045fcddb7977
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-08-17 12:39:22 -0400

    added controller and model files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# set up tables
+# CREATE TABLE users (id SERIAL, username VARCHAR(16), password VARCHAR(16), address TEXT);
+# CREATE TABLE employees (id SERIAL, user_id INT);
+# CREATE TABLE services (id SERIAL, service_type VARCHAR(155), service_price DECIMAL, employee_id INT);
+
+
+# set up data
+# INSERT INTO users (username, password, address) VALUES ('kate', 'kate', '100 fake dr, Columbus, Ohio');
+# INSERT INTO users (username, password, address) VALUES ('andy', 'andy', '100 fake dr, Columbus, Ohio');
+# INSERT INTO users (username, password, address) VALUES ('alex', 'alex', '100 fake dr, Columbus, Ohio');
+#
+# INSERT INTO employees (user_id) VALUES (1);
+# INSERT INTO employees (user_id) VALUES (2);
+#
+# INSERT INTO services (service_type, service_price, employee_id) VALUES ('Mow Lawn - 1/2Acre', 25.00, 1);
+# INSERT INTO services (service_type, service_price, employee_id) VALUES ('Edge Lawn', 12.00, 1);
+# INSERT INTO services (service_type, service_price, employee_id) VALUES ('Plant Flowers', 15.00, 1);
+#
+#
+# INSERT INTO services (service_type, service_price, employee_id) VALUES ('Mow Lawn - 1/2Acre', 28.00, 2);
+# INSERT INTO services (service_type, service_price, employee_id) VALUES ('Mulch', 15.00, 2);
