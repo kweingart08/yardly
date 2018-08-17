648a7c617e74172ce4934b481bdf1c14c822d4f9
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-08-17 12:42:18 -0400

    added rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# ******* routes for users *******
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
+
+  # ******* routes for employees *******
+  get '/employees', to: 'employees#index'
+  get '/employees/:id', to: 'employees#show'
+  post '/employees', to: 'employees#create'
+  delete '/employees/:id', to: 'employees#delete'
+  put '/employees/:id', to: 'employees#update'
+
+  # ******* routes for services *******
+  get '/services', to: 'services#index'
+  get '/services/:id', to: 'services#show'
+  post '/services', to: 'services#create'
+  delete '/services/:id', to: 'services#delete'
+  put '/services/:id', to: 'services#update'
+
 end
