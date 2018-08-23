dcadf5f2a253ba41df90d7d1f418d0369f33c603
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-22 19:03:13 -0500

    updated size of text on front screen

--- public/js/Front.js
+++ public/js/Front.js
@@ -5,7 +5,7 @@ class Front extends React.Component {
         <div className="logo-container">
           <img src="../images/yardly-logo.png"/>
         </div>
-        <p>Are you working hard or YARDLY working</p>
+        <p>Are you working hard or YARDLY working!</p>
         <p>All of your yard work needs done by someone else</p>
 
         <div className="front-buttons">
