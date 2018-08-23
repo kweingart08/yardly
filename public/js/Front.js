2085f217b64166753289d783cc8f365a78c07b8c
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-22 19:01:24 -0500

    added front page style

--- public/js/Front.js
+++ public/js/Front.js
@@ -2,22 +2,24 @@ class Front extends React.Component {
   render(){
     return (
       <div className="front-container">
-        <img src="../images/yardly-logo.png"/>
+        <div className="logo-container">
+          <img src="../images/yardly-logo.png"/>
+        </div>
         <p>Are you working hard or YARDLY working</p>
         <p>All of your yard work needs done by someone else</p>
 
         <div className="front-buttons">
-        <button
-          onClick={ () => this.props.toggleState( 'frontIsVisible', 'registerIsVisible' )}
-          toggleState={this.props.toggleState}
-        >
-        Register
-        </button>
+          <button
+            onClick={ () => this.props.toggleState( 'frontIsVisible', 'registerIsVisible' )}
+            toggleState={this.props.toggleState}
+          >
+          Register
+          </button>
 
-        <button
-        onClick={ () => this.props.toggleState( 'frontIsVisible', 'loginIsVisible' )}
-        toggleState={this.props.toggleState}
-        >SIGN IN</button>
+          <button
+          onClick={ () => this.props.toggleState( 'frontIsVisible', 'loginIsVisible' )}
+          toggleState={this.props.toggleState}
+          >SIGN IN</button>
 
         </div>
 
